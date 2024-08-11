ac3fe883393f0a00c847abf63bf2284f6a6bdf9c
aminakm123 <shamrifari@gmail.com>
2024-08-11 20:37:14 +0530

    routing updated

--- chat_app_backend/user/routing.py
+++ chat_app_backend/user/routing.py
@@ -1,4 +1,4 @@
-from django.urls import re_path
+from django.urls import path, re_path
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from .consumers import ChatConsumer  # Adjust the import path according to your project structure
@@ -6,7 +6,7 @@ from .consumers import ChatConsumer  # Adjust the import path according to your 
 application = ProtocolTypeRouter({
     "websocket": AuthMiddlewareStack(
         URLRouter([
-            re_path(r'ws/chat/(?P<room_name>\w+)/$', ChatConsumer.as_asgi()),
+            path("ws/chat/", ChatConsumer.as_asgi()),
         ])
     ),
 })
